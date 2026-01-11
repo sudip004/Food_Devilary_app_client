336d4c712a35ee07fba0512ebde5b51e6bdf5c5d
Sudip Basak <84569249+sudip004@users.noreply.github.com>
2026-01-11 17:09:41 +0530

    Update UserStore.js

--- src/store/UserStore.js
+++ src/store/UserStore.js
@@ -1,39 +1,35 @@
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
 import axios from 'axios';
 
-const UserStore = create(
-    persist(
-        (set) => ({
-            user: null,
-            userStoreTotal: 0,
-            
+const UserStore = create((set) => ({
+  user: null,
 
-            setuser: (userData) => set({ user: userData }),
-            setStoreCart: (cartData) => set({ storeCart: cartData }),
-            setUserStoreTotal: (total) => set({ userStoreTotal: total }),
+  setuser: (userData) => set({ user: userData }),
 
-            logout: async() =>{
-                await axios.post(`${import.meta.env.VITE_BASE_URL}/logout`, {}, { withCredentials: true });
-                set({ user: null });
-            },
+  logout: async () => {
+    await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/logout`,
+      {},
+      { withCredentials: true }
+    );
+    set({ user: null });
+  },
 
-            fetchUser: async () => {
-                try {
-                    const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/me`, { withCredentials: true });
-                    set({ user: response.data });
-                } catch (error) {
-                    set({ user: null });
-                }
-            }
-        }),
-        {
-            name:'user-storage'
-        }
-    )
-)
+  fetchUser: async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}/me`,
+        { withCredentials: true }
+      );
+      set({ user: response.data });
+    } catch {
+      set({ user: null });
+    }
+  }
+}));
 
 export default UserStore;
+
 
 
 
